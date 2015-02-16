eef7fcd649df44285f6914df4dff29f4b9783264
Zayik <DavidLeeAnderson1991@gmail.com>
2015-02-15 19:32:58 -0800

    Gogo correctly names commits!
    Prefabs now have Unit script attached to them.
    Selection manager has functions for adding/removing units to selection
    list.
    Network manager has a simple instantiate call for testing purposes. Will
    be changed tomorrow.

--- UASS_Client/Assets/Scripts/SelectionMgr.cs
+++ UASS_Client/Assets/Scripts/SelectionMgr.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
+
 public class SelectionMgr : MonoBehaviour {
 	List<GameObject> selectedUnits;
 	
@@ -13,16 +14,35 @@ public class SelectionMgr : MonoBehaviour {
 	void AddUnitToSelectedList(GameObject unit)
 	{
 		selectedUnits.Add(unit);
+		Unit stats = (Unit)unit.GetComponent("Unit");
+		stats.IsSelected = true;
+	}
+
+	void AddUnitsToSelectedList(List<GameObject> units)
+	{
+		foreach(GameObject unit in units)
+		{
+			selectedUnits.Add(unit);
+			Unit stats = (Unit)unit.GetComponent("Unit");
+			stats.IsSelected = true;
+		}
 	}
 
 	void RemoveUnitFromSelectedList(GameObject unit)
 	{
 		selectedUnits.Remove(unit);
+		Unit stats = (Unit)unit.GetComponent("Unit");
+		stats.IsSelected = false;
 	}
 
 	void RemoveAllUnits()
 	{
-
+		foreach(GameObject unit in selectedUnits)
+		{
+			Unit stats = (Unit)unit.GetComponent("Unit");
+			stats.IsSelected = false;
+		}
+		selectedUnits.Clear ();
 	}
 
 	// Update is called once per frame
