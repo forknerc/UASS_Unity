e94488fa6d4ceb361e32eb145b09bdb5354fe662
unknown <cforkner@05620396GPK02.rd.unr.edu>
2015-03-05 14:23:27 -0800

    made waypoint motion smoother

--- UASS_Client/Assets/Scripts/WayPointMotion.cs
+++ UASS_Client/Assets/Scripts/WayPointMotion.cs
@@ -3,19 +3,22 @@ using System.Collections;
 
 public class WayPointMotion : MonoBehaviour {
 
-	public float offset;
+	public float offsetNum;
 	public float amplitude;
+	public float heightOffset;
 	public float freq;
+	private float offset;
 
 	// Use this for initialization
 	void Start () {
 	
+		offset = Mathf.PI * offsetNum / 4;
+
 	}
 	
 	// FixedUpdate is called once per physics call
 	void FixedUpdate () {
 	
-		transform.position = new Vector3(0.0f, Mathf.Abs((Mathf.Sin(offset + Time.time * freq)) * amplitude), 0.0f);
-
+		transform.position = new Vector3(0.0f, ((Mathf.Sin(offset + Time.time * freq)) * amplitude) + heightOffset, 0.0f);
 	}
 }
