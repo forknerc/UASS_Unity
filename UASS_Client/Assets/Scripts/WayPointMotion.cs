078a8d385ed0bb0153734d26a7de6962029f31cd
unknown <cforkner@05620396GPK02.rd.unr.edu>
2015-03-05 14:00:43 -0800

    added waypoint with animation

---
+++ UASS_Client/Assets/Scripts/WayPointMotion.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WayPointMotion : MonoBehaviour {
+
+	public float offset;
+	public float amplitude;
+	public float freq;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// FixedUpdate is called once per physics call
+	void FixedUpdate () {
+	
+		transform.position = new Vector3(0.0f, Mathf.Abs((Mathf.Sin(offset + Time.time * freq)) * amplitude), 0.0f);
+
+	}
+}
