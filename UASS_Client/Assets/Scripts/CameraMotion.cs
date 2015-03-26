e1e9a263e906e06f63ddeae8cc565cc378033778
unknown <cforkner@05620396GPK02.rd.unr.edu>
2015-03-26 12:46:47 -0700

    added HREL model and scaled units

--- UASS_Client/Assets/Scripts/CameraMotion.cs
+++ UASS_Client/Assets/Scripts/CameraMotion.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CameraMotion : MonoBehaviour {
 
-	public float trasnlationSpeed;
+	public float translationSpeed;
 	public float zoomSpeed;
 	public float rotateSpeed; // soon^tm
 
@@ -15,20 +15,26 @@ public class CameraMotion : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
-		float vert, horiz, zoom;	
+		float vert, horiz, zoom, rotate;	
 
 		if((vert = Input.GetAxis("Vertical")) != 0)
 		{
-			transform.position += Vector3.forward * vert * trasnlationSpeed * Time.deltaTime;
+			transform.position += Vector3.forward * vert * translationSpeed * Time.deltaTime;
 		}
 
 		if((horiz = Input.GetAxis("Horizontal")) != 0)
 		{
-			transform.position += Vector3.right * horiz * trasnlationSpeed * Time.deltaTime;
+			transform.position += Vector3.right * horiz * translationSpeed * Time.deltaTime;
 		}
 		if((zoom = Input.GetAxis("Zoom")) != 0)
 		{
 			transform.position += transform.forward * zoom * zoomSpeed * Time.deltaTime;
+		}
+		if((rotate = Input.GetAxis("Rotate")) != 0)
+		{
+			Vector3 temp = transform.rotation.eulerAngles;
+			temp.x += rotate * rotateSpeed * Time.deltaTime;
+			transform.rotation = Quaternion.Euler(temp);
 		}
 	}
 }
